bcfa92daa2c469feccbb2641ef323b897096f2a3
Jakriya12 <116625734+Jakriya12@users.noreply.github.com>
2023-04-18 13:17:10 +0530

    main

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import logoImg from "../../images/logo.png";
+import {HiOutlineMenuAlt3} from "react-icons/hi";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleNavbar = () => setToggleMenu(!toggleMenu);
+
+  return (
+    <nav className='navbar' id = "navbar">
+      <div className='container navbar-content flex'>
+        <div className='brand-and-toggler flex flex-sb'>
+          <Link to = "/" className='navbar-brand flex'>
+            <img src = {logoImg} alt = "site logo" />
+            <div class='text-uppercase'>Library  </div>
+            
+          
+            
+          </Link>
+          <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
+            <HiOutlineMenuAlt3 size = {35} style = {{
+              color: `${toggleMenu ? "#fff" : "#010101"}`
+            }} />
+          </button>
+        </div>
+
+        <div className={toggleMenu ? "navbar-collapse show-navbar-collapse" : "navbar-collapse"}>
+         
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
